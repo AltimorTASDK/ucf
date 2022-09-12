0831ecfa357117db13683082ce7be3f991fbb591
AltimorTASDK <altimormc@gmail.com>
2022-09-12 19:41:54 -0400

    fix the epic shield drop through the stage bug

--- src/shielddrop_extended/shielddrop_extended.cpp
+++ src/shielddrop_extended/shielddrop_extended.cpp
@@ -5,24 +5,37 @@
 
 register HSD_GObj *gobj asm("r31");
 
-static bool check_sdrop_extension(const vec2 &stick)
+[[gnu::noinline]] static bool check_coord(const vec2 &stick)
 {
 	// Must be -6125 or below along the rim, adjusted to prevent an ICs desync
 	return stick.y <= FP(popo_to_nana(-.6125f)) && is_rim_coord(stick);
 }
 
+static bool check_sdrop_extension(const Player *player)
+{
+	return player->input.stick_y_hold_time < 3 &&
+	       check_coord(player->input.stick) &&
+	       check_coord(player->input.last_stick);
+}
+
 static void gecko_entry()
 {
-	register bool success asm("r0");
+	// Skip if vanilla check succeeded
+	asm goto("cror   eq, lt, eq\r\n"
+	         "beq    %l[end]"
+	         : : : : end);
+
 	register Player *player asm("r4");
 
-	success = player->input.stick_y_hold_time < 3 &&
-	          check_sdrop_extension(player->input.stick) &&
-	          check_sdrop_extension(player->input.last_stick);
+	if (check_sdrop_extension(player))
+		asm("crset eq");
+	else
+		asm("crclr eq");
 
-	FORCE_WRITE(success);
+end:
+	return;
 }
 
 GAME_FUNC bool Interrupt_Pass_OoS(HSD_GObj *gobj);
 
-GECKO_INIT_PIC(Interrupt_Pass_OoS+0x80, gecko_entry, "r30");
+GECKO_INIT_PIC(Interrupt_Pass_OoS+0x38, gecko_entry, "r30");
