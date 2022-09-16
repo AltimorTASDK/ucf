f080f2535712ef9d5ed1c9306bec52bcab494d94
AltimorTASDK <altimormc@gmail.com>
2022-09-15 23:46:49 -0400

    make cr struct a union for raw access

--- include/util/powerpc.h
+++ include/util/powerpc.h
@@ -6,12 +6,15 @@ namespace ppc {
 
 // Condition register fields for crN
 template<int N>
-struct cr {
-	u32 : 4 * N;
-	u32 lt : 1;
-	u32 gt : 1;
-	u32 eq : 1;
-	u32 so : 1;
+union cr {
+	struct {
+		u32 : 4 * N;
+		u32 lt : 1;
+		u32 gt : 1;
+		u32 eq : 1;
+		u32 so : 1;
+	};
+	u32 raw;
 };
 
 } // namespace ppc
